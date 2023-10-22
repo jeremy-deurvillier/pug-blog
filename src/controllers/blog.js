43350d06fd0b9747d0efebc90ce8b69f91a84b1b
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-10-22 23:15:47 +0200

    Add basic navigation

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -2,6 +2,6 @@ import express from 'express'
 
 const router = express.Router()
 
-router.get('/', (req, res) => res.render('blog/home', {title: 'Blog'}))
+router.get('/', (req, res) => res.render('blog/home', {title: 'Blog', description: 'Blog de Jérémy Deurvillier, Développeur Web et Web Mobile.'}))
 
 export default router
