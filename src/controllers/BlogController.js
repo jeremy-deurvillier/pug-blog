e4a70cbb490d616150076610a580875e4eca8b30
jeremy-deurvillier <jerem.deurvil@gmail.com>
2023-10-24 22:40:01 +0200

    Add single post card

--- src/controllers/BlogController.js
+++ src/controllers/BlogController.js
@@ -1,14 +1,48 @@
 const Blog = {}
 
+Blog.posts = [
+    {
+        title: 'Drag and Drop',
+        description: 'Implémentons un simple système de drag and drop en POO.',
+        slug: 'drag-and-drop'
+    },
+    {
+        title: 'Drag and Drop',
+        description: 'Implémentons un simple système de drag and drop en POO.',
+        slug: 'drag-and-drop'
+    },
+    {
+        title: 'Drag and Drop',
+        description: 'Implémentons un simple système de drag and drop en POO.',
+        slug: 'drag-and-drop'
+    },
+    {
+        title: 'Drag and Drop',
+        description: 'Implémentons un simple système de drag and drop en POO.',
+        slug: 'drag-and-drop'
+    },
+    {
+        title: 'Drag and Drop',
+        description: 'Implémentons un simple système de drag and drop en POO.',
+        slug: 'drag-and-drop'
+    }
+]
+
 Blog.list = (req, res) => {
     res.render(
         'blog/home', 
         {
             title: 'Blog', 
             subtitle: 'Liste des articles.',
-            description: 'Blog de Jérémy Deurvillier, Développeur Web et Web Mobile.'
+            description: 'Blog de Jérémy Deurvillier, Développeur Web et Web Mobile.',
+            posts: Blog.posts
         }
     )
+}
+
+Blog.single = (req, res) => {
+    console.log(req.originalUrl)
+    res.send('ok')
 }
 
 export default Blog
--- src/routers/BlogRouter.js
+++ src/routers/BlogRouter.js
@@ -4,5 +4,6 @@ import Blog from '../controllers/BlogController.js'
 const router = express.Router()
 
 router.get('/', Blog.list)
+router.get('/posts/:slug', Blog.single)
 
 export default router
